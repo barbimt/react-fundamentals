b63ff52d01d7d6e9a07dcab288080ef178374af8
Barbi Torres <barbi.mtorres@gmail.com>
2021-12-02 14:30:02 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+import React from "react";
+import ReactDOM from "react-dom";
 
-// const container = document.getElementById('app');
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
+import Badge from "./components/Badge";
 
-// container.appendChild(element);
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
-
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(
+  <Badge
+    firstName="Marcio"
+    lastName="Alico"
+    jobTitle="IoS Engineer"
+    twitter="marcioalico"
+    avatarUrl="https://secure.gravatar.com/avatar/6f334768c1f0ad84f3a608bb79d32b9c"
+  />,
+  container
+);
