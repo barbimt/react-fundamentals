2eb9a43e1598e26eb3212053f581f987f8016402
Barbi Torres <barbi.mtorres@gmail.com>
2021-12-14 17:29:41 -0300

    add BadgesList and data

---
+++ src/components/BadgesList.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class BadgesList extends Component {
+  render() {
+    return (
+      <ul className="list-unstyled">
+        {this.props.badges.map((badge) => {
+          return (
+            <li key={badge.id}>
+              <p>
+                {badge.firstName} {badge.lastName}
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+export default BadgesList;
